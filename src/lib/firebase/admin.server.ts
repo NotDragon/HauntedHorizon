d717c83ca45c4d64320fd9cbe171c4c4db1d6115
NotDragon <mixalis.m.hadjittofi@gmail.com>
2024-10-23 17:55:14 +0300

    Phone COmpatability

--- src/lib/firebase/admin.server.ts
+++ src/lib/firebase/admin.server.ts
@@ -18,8 +18,7 @@ function makeApp() {
 	}
 
 	const app = initializeApp(
-		dev
-			? {
+		 {
 					credential: cert({
 						privateKey: PRIVATE_FIREBASE_ADMIN_KEY,
 						clientEmail: PRIVATE_FIREBASE_ADMIN_CLIENT_EMAIL,
@@ -29,7 +28,7 @@ function makeApp() {
 					databaseURL: `https://${PUBLIC_PROJECTID}.firebaseio.com`,
 					storageBucket: PUBLIC_STORAGEBUCKET
 			  }
-			: undefined,
+			,
 		PUBLIC_PROJECTID
 	)
 	const firestore = getFirestore(app)
